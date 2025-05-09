29f140d47d16664819cf7136aed37da945cef61a
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-05-09 11:00:31 +0530

    updated

--- src/view/Index.jsx
+++ src/view/Index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Index = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Index</h1>
+      <button onClick={()=>navigate('/login')}>login</button>
     </div>
   )
 }
